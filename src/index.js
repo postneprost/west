53d2fab476e360417a76de04f103f9c7bcb75537
postneprost <postneprost@yandex.ru>
2020-05-24 16:34:04 +0500

    create Dog and Duck

--- src/index.js
+++ src/index.js
@@ -3,9 +3,24 @@ import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
 
+class Duck extends Card {
+    constructor() {
+        super('Мирная утка', 2);
+    }
+
+    quacks = function () { console.log('quack') };
+    swims = function () { console.log('float: both;') };
+}
+
+class Dog extends Card {
+    constructor() {
+        super('Пес-бандит', 3);
+    }
+}
+
 // Отвечает является ли карта уткой.
 function isDuck(card) {
-    return card && card.quacks && card.swims;
+    return card instanceof Duck;
 }
 
 // Отвечает является ли карта собакой.
@@ -27,30 +42,16 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
-
-
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
-}
-
-
-// Основа для собаки.
-function Dog() {
-}
-
-
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck()
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog()
 ];
 
 
